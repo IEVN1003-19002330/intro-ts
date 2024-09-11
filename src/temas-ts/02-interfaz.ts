2a53b08cf6011216b5405364571222d755998369
Eva Montserrat <19002330@alumnos.utleon.edu.mx>
2024-09-10 19:13:47 -0600

    segundo commit del martes 10/09/2024

--- src/temas-ts/02-interfaz.ts
+++ src/temas-ts/02-interfaz.ts
@@ -14,4 +14,14 @@ const alumno:Alumno={
     email:'mario@gmail.com'
 }
 
-console.log(alumno)
+console.table(alumno);
+
+let mascotas=['perro','gato','perico'];
+mascotas[1]='nuevo gato';
+mascotas.push('leon');
+console.log(mascotas);
+
+let tem:(string|number)[]=[];
+tem.push(11);
+tem.push('hola');
+console.log(tem);
